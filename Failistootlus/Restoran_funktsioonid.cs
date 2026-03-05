aa73a1c9a6d37f58cf88252fc325709af127a809
opilane <opilane@HP-CZC2349HTY>
2026-03-05 14:28:07 +0200

    töö tundis

--- Failistootlus/Restoran_funktsioonid.cs
+++ Failistootlus/Restoran_funktsioonid.cs
@@ -76,7 +76,7 @@ namespace Failistootlus
                         {
                             rets.Add(rida);
 
-                            }//
+                            }//  
                     }
 
                     catch (Exception)
